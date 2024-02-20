7ca71062943579cc9780a8e5f93d27f6ea183317
vinh <unforgivinh@gmail.com>
2024-02-20 15:18:59 +0100

    feat: Delegation with Kotlin

--- playground-kotlin-delegation/src/main/kotlin/io/unforgivinh/playground/kotlin/delegation/DelegationWithKotlin.kt
+++ playground-kotlin-delegation/src/main/kotlin/io/unforgivinh/playground/kotlin/delegation/DelegationWithKotlin.kt
@@ -1,9 +1,30 @@
 package io.unforgivinh.playground.kotlin.delegation
 
-class DelegationWithKotlin {
+interface Behavior {
+
+    val message: String
+
+    fun print(): String
+}
+
+class BehaviorDelegate(x: Int) : Behavior {
+
+    override val message = "BehaviorDelegate: x = $x"
+
+    override fun print(): String {
+        println(message)
+        return message
+    }
+}
+
+class Derived(b: Behavior) : Behavior by b {
+    // This property is not accessed from b's implementation of `print`
+    override val message = "Message of Derived"
 }
 
 fun main() {
-
-    println("Hello World, delegation")
+    val b = BehaviorDelegate(10)
+    val derived = Derived(b)
+    derived.print()
+    println(derived.message)
 }
